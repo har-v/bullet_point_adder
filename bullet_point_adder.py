71390e388ac0799700e4cfa67e75dbed624b5335
Alice Harvey <66881641+har-v@users.noreply.github.com>
2020-08-16 22:16:38 +0100

    Update bullet_point_adder.py

--- bullet_point_adder.py
+++ bullet_point_adder.py
@@ -2,11 +2,6 @@
 # Adds bullet points to the start of each line that has been copied to the
 # clipboard.
 
-""" Instructions for use:
-    1. Open command prompt
-    2. Enter @py.exe C:/[path where you saved this file]\password_locker.py [account name]
-"""
-
 import pyperclip
 text = pyperclip.paste()
 
